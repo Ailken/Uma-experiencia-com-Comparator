fae2120574f5f949257014dede50919e3dc2810f
ailken <ailkencf@gmail.com>
2024-10-07 15:55:05 -0300

    resolução com comparator dentro da classe product

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,24 @@
 package application;
 
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Program {
+
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<Product>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        Collections.sort(list);
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
 }
